cf28e31b2eb7cf3ab4f0b538293cf5a60fb432ae
lomayd <jinho10167@gmail.com>
2023-03-16 16:57:51 +0900

    feat: DBMS Configuration

---
+++ src/main/java/lomayd/DBMSLabRDBMS/api/global/datasource/config/OracleConfig.java
@@ -0,0 +1,65 @@
+package lomayd.DBMSLabRDBMS.api.global.datasource.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+
+@Configuration
+@PropertySource({ "classpath:application.yml" })
+@EnableJpaRepositories(
+        basePackages = "lomayd.DBMSLabRDBMS.api.domain.user.repository",
+        entityManagerFactoryRef = "oracleEntityManager",
+        transactionManagerRef = "oracleTransactionManager"
+)
+public class OracleConfig {
+    @Autowired
+    private Environment env;
+
+    @Bean
+    @Primary
+    public LocalContainerEntityManagerFactoryBean oracleEntityManager() {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(oracleDataSource());
+
+        //Entity 패키지 경로
+        em.setPackagesToScan(new String[] { "lomayd.DBMSLabRDBMS.api.domain.user" });
+
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        em.setJpaVendorAdapter(vendorAdapter);
+
+        //Hibernate 설정
+        HashMap<String, Object> properties = new HashMap<>();
+        properties.put("hibernate.hbm2ddl.auto",env.getProperty("hibernate.hbm2ddl.auto"));
+        properties.put("hibernate.dialect",env.getProperty("hibernate.dialect"));
+        em.setJpaPropertyMap(properties);
+        return em;
+    }
+
+    @Primary
+    @Bean
+    @ConfigurationProperties(prefix="spring.oracle")
+    public DataSource oracleDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Primary
+    @Bean
+    public PlatformTransactionManager oracleTransactionManager() {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(oracleEntityManager().getObject());
+        return transactionManager;
+    }
+}
